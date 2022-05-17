8121e15f5a0b62587128a1d99f21ee7ac1992461
mpuchkova <puchkovamm2001@mail.ru>
2022-05-17 22:38:36 +0300

    task-template

--- public/script.js
+++ public/script.js
@@ -121,3 +121,234 @@ async function getUserPlaylists(){
         console.error('Error',err);
     }
 };
+
+const $albums = document.querySelector('#albums');
+
+async function getToken() {
+  try {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      body: 'grant_type=client_credentials',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic ' + btoa(client_id + ':' + client_secret),
+      },
+    });
+    const token = await response.json();
+    return token.access_token;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function getAlbums() {
+  const limit = 4;
+  const token = await getToken();
+  try {
+    const response = await fetch(
+      `https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/albums?include_groups=appears_on&market=ES&limit=${limit}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        },
+      },
+    );
+    return response.json();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const albums = getAlbums();
+
+albums.then((data) => {
+  console.log(data.items);
+  data.items.forEach((item) => {
+    console.log(item);
+    $albums.insertAdjacentHTML(
+      'beforeend',
+      `<div class="card">
+
+    <img class="card_image" src="${item.images[0].url}" alt="${item.name}" />
+    <div class="card_title">${item.name}</div>
+    <div class="card_autor">${item.release_date}</div>
+
+    </div>`,
+    );
+  });
+});
+
+
+
+const $artist = document.querySelector('#artist');
+
+async function getArtist() {
+  const token = await getToken();
+  const id="smedjan";
+  const limit=4;
+  try {
+    const response = await fetch(
+      `https://api.spotify.com/v1/users/${id}/playlists?limit=${limit}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        },
+      },
+    );
+    return response.json();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const artist = getArtist();
+
+artist.then((data) => {
+  console.log(data.items);
+  data.items.forEach((item) => {
+    console.log(item);
+    $artist.insertAdjacentHTML(
+      'beforeend',
+      `<div class="card">
+
+    <img class="card_image" src="${item.images[0].url}" alt="${item.name}" />
+    <div class="card_title">${item.name}</div>
+    <div class="card_autor">${item.description}</div>
+ 
+    </div>`,
+    );
+  });
+});
+
+
+
+
+const $i = document.querySelector('#i');
+
+async function geti() {
+  const token = await getToken();
+  const id="31dfsiux6jwrxdnm6jf6urevulhy";
+  const limit=4;
+  try {
+    const response = await fetch(
+      `https://api.spotify.com/v1/users/${id}/playlists?limit=${limit}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        },
+      },
+    );
+    return response.json();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const i = geti();
+
+i.then((data) => {
+  console.log(data.items);
+  data.items.forEach((item) => {
+    console.log(item);
+    $i.insertAdjacentHTML(
+      'beforeend',
+      `<div class="card">
+ 
+    <img class="card_image" src="${item.images[0].url}" alt="${item.name}" />
+    <div class="card_title">${item.name}</div>
+    <div class="card_autor">${item.description}</div>
+
+    </div>`,
+    );
+  });
+});
+
+
+
+const $song = document.querySelector('#song');
+
+async function getsong() {
+  const token = await getToken();
+  const id="spotify";
+  const limit=6;
+  try {
+    const response = await fetch(
+      `https://api.spotify.com/v1/users/${id}/playlists?limit=${limit}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        },
+      },
+    );
+    return response.json();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const song = getsong();
+
+song.then((data) => {
+  console.log(data.items);
+  data.items.forEach((item) => {
+    console.log(item);
+    $song.insertAdjacentHTML(
+      'beforeend',
+      `<div class="box">
+    
+    <img class="box_image" src="${item.images[0].url}" alt="${item.name}" />
+    <div class="box_title">${item.name}</div>
+    </div>`,
+    );
+  });
+});
+
+const $forme = document.querySelector('#forme');
+
+async function getforme() {
+  const token = await getToken();
+  const id="4";
+  const limit=4;
+  try {
+    const response = await fetch(
+      `https://api.spotify.com/v1/users/${id}/playlists?limit=${limit}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token,
+        },
+      },
+    );
+    return response.json();
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+const forme = getforme();
+
+forme.then((data) => {
+  console.log(data.items);
+  data.items.forEach((item) => {
+    console.log(item);
+    $forme.insertAdjacentHTML(
+      'beforeend',
+      `<div class="card">
+
+    <img class="card_image" src="${item.images[0].url}" alt="${item.name}" />
+    <div class="card_title">${item.name}</div>
+    <div class="card_autor">${item.description}</div>
+   
+    </div>`,
+    );
+  });
+});
