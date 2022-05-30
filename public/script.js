64003b27ded9a270c4e4ec9ec9a790b425c6b09f
mpuchkova <puchkovamm2001@mail.ru>
2022-05-30 22:44:54 +0300

    task-template-js

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-var redirect_uri = "http://localhost:3000/";
+const redirect_uri = "http://localhost:3000/";
 
 const client_id = '4aaa74b856d84cfcb24ddbf77cc57935';
 const client_secret = '8a98aad6afc2450a83c2496e2abc395e';
@@ -11,68 +11,8 @@ const PLAYLISTS = "https://api.spotify.com/v1/me/playlists";
 var access_token = null;
 var refresh_token = null;
 
-function requestAuthorization(){
-    let url = AUTHORIZE;
-    url += "?client_id=" + client_id;
-    url += "&response_type=code";
-    url += "&redirect_uri=" + encodeURI(redirect_uri);
-    url += "&show_dialog=true";
-    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private playlist-modify-private";
-    window.location.href = url; 
-}
 
-function onPageLoad(){
-    if ( window.location.search.length > 0 ){
-        handleRedirect();
-    }
-}
-function handleRedirect(){
-    let code = getCode();
-    fetchAccessToken( code );
-    window.history.pushState("", "", redirect_uri);
-}
-
-function fetchAccessToken( code ){
-    let body = "grant_type=authorization_code";
-    body += "&code=" + code; 
-    body += "&redirect_uri=" + encodeURI(redirect_uri);
-    body += "&client_id=" + client_id;
-    body += "&client_secret=" + client_secret;
-    callAuthorizationApi(body);
-}
-
-function callAuthorizationApi(body){
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", TOKEN, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
-    xhr.send(body);
-    xhr.onload = handleAuthorizationResponse;
-}
-
-function handleAuthorizationResponse(){
-    if ( this.status == 200 ){
-        var data = JSON.parse(this.responseText);
-        console.log(data);
-        var data = JSON.parse(this.responseText);
-        if ( data.access_token != undefined ){
-            access_token = data.access_token;
-            localStorage.setItem("access_token", access_token);
-        }
-        if ( data.refresh_token  != undefined ){
-            refresh_token = data.refresh_token;
-            localStorage.setItem("refresh_token", refresh_token);
-        }
-        onPageLoad();
-    }
-    else {
-        console.log(this.responseText);
-        alert(this.responseText);
-    }
-}
-
-const $albums = document.querySelector('#albums');
-
+///справится с записью в куки не смогла
 async function getToken() {
   try {
     const response = await fetch('https://accounts.spotify.com/api/token', {
@@ -90,33 +30,38 @@ async function getToken() {
   }
 }
 
-async function getAlbums() {
-  const limit = 4;
+const request = async (url, method) => {
   const token = await getToken();
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer` + token,
+  };
+
   try {
-    const response = await fetch(
-      `https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/albums?include_groups=appears_on&market=ES&limit=${limit}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-        },
-      },
-    );
+    const response = await fetch(url, {
+      method: method,
+      headers: headers,
+    });
+
     return response.json();
-  } catch (e) {
-    console.log(e);
+  } catch (err) {
+    console.error('Error', err);
   }
+};
+
+
+const ALBUMS = document.querySelector('#albums');
+
+async function getAlbums() {
+  const var_url=`https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/albums?include_groups=appears_on&market=ES&limit=4`;
+  return await request(var_url,'GET');
 }
 
 const albums = getAlbums();
 
 albums.then((data) => {
-  console.log(data.items);
   data.items.forEach((item) => {
-    console.log(item);
-    $albums.insertAdjacentHTML(
+    ALBUMS.insertAdjacentHTML(
       'beforeend',
       `<div class="card">
 
@@ -129,38 +74,18 @@ albums.then((data) => {
   });
 });
 
-
-
-const $artist = document.querySelector('#artist');
+const ARTIST = document.querySelector('#artist');
 
 async function getArtist() {
-  const token = await getToken();
-  const id="smedjan";
-  const limit=4;
-  try {
-    const response = await fetch(
-      `https://api.spotify.com/v1/users/${id}/playlists?limit=${limit}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-        },
-      },
-    );
-    return response.json();
-  } catch (e) {
-    console.log(e);
-  }
+  const var_url=`https://api.spotify.com/v1/users/smedjan/playlists?limit=4`;
+  return await request(var_url,'GET');
 }
 
 const artist = getArtist();
 
 artist.then((data) => {
-  console.log(data.items);
   data.items.forEach((item) => {
-    console.log(item);
-    $artist.insertAdjacentHTML(
+    ARTIST.insertAdjacentHTML(
       'beforeend',
       `<div class="card">
 
@@ -174,36 +99,18 @@ artist.then((data) => {
 });
 
 
-const $i = document.querySelector('#i');
+const ABOUTME = document.querySelector('#i');
 
-async function geti() {
-  const token = await getToken();
-  const id="31dfsiux6jwrxdnm6jf6urevulhy";
-  const limit=4;
-  try {
-    const response = await fetch(
-      `https://api.spotify.com/v1/users/${id}/playlists?limit=${limit}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-        },
-      },
-    );
-    return response.json();
-  } catch (e) {
-    console.log(e);
-  }
+async function getI() {
+  const var_url=`https://api.spotify.com/v1/users/31dfsiux6jwrxdnm6jf6urevulhy/playlists?limit=4`;
+  return await request(var_url,'GET');
 }
 
-const i = geti();
+const i = getI();
 
 i.then((data) => {
-  console.log(data.items);
   data.items.forEach((item) => {
-    console.log(item);
-    $i.insertAdjacentHTML(
+    ABOUTME.insertAdjacentHTML(
       'beforeend',
       `<div class="card">
  
@@ -218,36 +125,18 @@ i.then((data) => {
 
 
 
-const $song = document.querySelector('#song');
+const SONG = document.querySelector('#song');
 
-async function getsong() {
-  const token = await getToken();
-  const id="spotify";
-  const limit=6;
-  try {
-    const response = await fetch(
-      `https://api.spotify.com/v1/users/${id}/playlists?limit=${limit}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-        },
-      },
-    );
-    return response.json();
-  } catch (e) {
-    console.log(e);
-  }
+async function getSong() {
+  const var_url=`https://api.spotify.com/v1/users/spotify/playlists?limit=6`;
+  return await request(var_url,'GET');
 }
 
-const song = getsong();
+const song = getSong();
 
 song.then((data) => {
-  console.log(data.items);
   data.items.forEach((item) => {
-    console.log(item);
-    $song.insertAdjacentHTML(
+    SONG.insertAdjacentHTML(
       'beforeend',
       `<div class="box">
     
@@ -258,36 +147,18 @@ song.then((data) => {
   });
 });
 
-const $forme = document.querySelector('#forme');
+const FORM = document.querySelector('#form');
 
-async function getforme() {
-  const token = await getToken();
-  const id="4";
-  const limit=4;
-  try {
-    const response = await fetch(
-      `https://api.spotify.com/v1/users/${id}/playlists?limit=${limit}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + token,
-        },
-      },
-    );
-    return response.json();
-  } catch (e) {
-    console.log(e);
-  }
+async function getForm() {
+  const var_url=`https://api.spotify.com/v1/users/4/playlists?limit=4`;
+  return await request(var_url,'GET');
 }
 
-const forme = getforme();
+const form = getForm();
 
-forme.then((data) => {
-  console.log(data.items);
+form.then((data) => {
   data.items.forEach((item) => {
-    console.log(item);
-    $forme.insertAdjacentHTML(
+    FORM.insertAdjacentHTML(
       'beforeend',
       `<div class="card">
 
